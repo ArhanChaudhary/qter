42a0fde971f37ac500966b437f08c635f5b6db08
Arhan Chaudhary <arhan.ch@gmail.com>
2024-11-15 00:12:56 +0000

    ultra efficient multiply program

--- example-programs/multiply.c
+++ example-programs/multiply.c
@@ -4,9 +4,6 @@
 #include <time.h>
 #include <assert.h>
 
-int count = 0;
-int count2 = 0;
-
 void move_const(int n, int *to)
 {
 move_const_loop:
@@ -17,8 +14,6 @@ move_const_loop:
     *to -= 1;
     goto move_const_loop;
 after_move_const_loop:
-    if (n != 0) {
-    }
     *to += n;
 }
 
@@ -70,16 +65,17 @@ reduce_a_loop:
 
 void multiply(int *a, int *b, int *c)
 {
+    move_const(0, c);
+    if (*a == 0)
+    {
+        move_const(0, b);
+        return;
+    }
     if (*b == 0)
     {
         move_const(0, a);
         return;
     }
-    if (*a == 0)
-    {
-        return;
-    }
-    move_const(0, c);
 reduce_by_2:
     if (*b == 0)
     {
@@ -91,35 +87,32 @@ reduce_by_2:
     *c += 1;
     if (*b == 0)
     {
-        move(c, b);
         goto reduce_by_3;
     }
     *b -= 1;
     *c += 1;
     goto reduce_by_2;
 reduce_by_3:
-    if (*b == 0)
+    if (*c == 0)
     {
-        move(c, b);
-        reduce_problem(a, b, c, 3);
+        move(b, c);
+        reduce_problem(a, c, b, 3);
         goto reduce_by_3;
     }
-    *b -= 1;
-    *c += 1;
-    if (*b == 0)
+    *c -= 1;
+    *b += 1;
+    if (*c == 0)
     {
-        move(c, b);
         goto reduce_by_5;
     }
-    *b -= 1;
-    *c += 1;
-    if (*b == 0)
+    *c -= 1;
+    *b += 1;
+    if (*c == 0)
     {
-        move(c, b);
         goto reduce_by_5;
     }
-    *b -= 1;
-    *c += 1;
+    *c -= 1;
+    *b += 1;
     goto reduce_by_3;
 reduce_by_5:
     if (*b == 0)
@@ -132,89 +125,55 @@ reduce_by_5:
     *c += 1;
     if (*b == 0)
     {
-        move(c, b);
         goto reduce_until_1;
     }
     *b -= 1;
     *c += 1;
     if (*b == 0)
     {
-        move(c, b);
         goto reduce_until_1;
     }
     *b -= 1;
     *c += 1;
     if (*b == 0)
     {
-        move(c, b);
         goto reduce_until_1;
     }
     *b -= 1;
     *c += 1;
     if (*b == 0)
     {
-        move(c, b);
         goto reduce_until_1;
     }
     *b -= 1;
     *c += 1;
     goto reduce_by_5;
 reduce_until_1:
-    *b -= 1;
-    if (*b == 0)
+    *c -= 1;
+    if (*c == 0)
     {
         return;
     }
-    *b += 1;
-    reduce_problem(a, b, c, 7);
-    reduce_problem(a, b, c, 11);
+    *c += 1;
+    reduce_problem(a, c, b, 7);
+    reduce_problem(a, c, b, 11);
     goto reduce_until_1;
 }
 
-typedef struct result {
-    char *comp;
-    int count;
-    int count2;
-} result_t;
-
 int main()
 {
-    result_t results[8100];
-    char buf[100];
-    srand(time(NULL));
     for (int i = 0; i < 90; i++)
     {
         for (int j = 0; j < 90; j++)
         {
             int a = i;
             int b = j;
-            int c = rand() % 90;
-            count = 0;
-            count2 = 0;
+            int c = 0;
             multiply(&a, &b, &c);
-            snprintf(buf, sizeof(buf), "%d * %d = %d", i, j, a);
-            results[i * 90 + j].comp = strdup(buf);
-            results[i * 90 + j].count = count;
-            results[i * 90 + j].count2 = count2;
+            printf("%d * %d = %d", i, j, a);
             assert(((long long)i * (long long)j) % 90 == a);
+            assert(b == 0);
+            assert(c == 0);
         }
     }
-
-    // insertion sort by count
-    for (int i = 1; i < 8100; i++)
-    {
-        result_t key = results[i];
-        int j = i - 1;
-        while (j >= 0 && results[j].count > key.count)
-        {
-            results[j + 1] = results[j];
-            j--;
-        }
-        results[j + 1] = key;
-    }
-    for (int i = 0; i < 8100; i++)
-    {
-        printf("%s: %d additions, %d comparisons\n", results[i].comp, results[i].count, results[i].count2);
-    }
-    return 0;
 }
