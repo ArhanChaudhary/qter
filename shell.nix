10181c70c7a909e7e29a48c197e6f4f206487305
Henry Rovnyak <h.rovnyak@proton.me>
2024-12-28 04:16:56 -0500

    nix-direnv + pin oxalica version

--- shell.nix
+++ shell.nix
@@ -1,7 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  rust_overlay = import (builtins.fetchTarball {
+    url = "https://github.com/oxalica/rust-overlay/archive/a16b9a7cac7f4d39a84234d62e91890370c57d76.tar.gz";
+    sha256 = "sha256:05xyk469bj6zkvkk4gmc58rkiyavamn4xhfglwkdqlanqiyfwdfz";
+  });
   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
   rust = pkgs.rust-bin.stable."1.81.0".default.override {
     extensions = [ "rust-src" ];
