e220947b8fed263f2072d66cf5ab87ba822befee
Arhan Chaudhary <arhan.ch@gmail.com>
2025-02-22 16:59:32 -0500

    bump nightly version for simd64 swizzle

--- shell.nix
+++ shell.nix
@@ -10,7 +10,7 @@ let
     }
   );
   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-  rust = pkgs.rust-bin.nightly."2025-01-28".default.override {
+  rust = pkgs.rust-bin.nightly."2025-02-22".default.override {
     extensions = [ "rust-src" "rust-analyzer" ];
   };
 in
