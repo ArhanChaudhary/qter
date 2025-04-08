65cb8c637cbd55d25b0e90e13f2fc1cfa3d68d9f
Arhan Chaudhary <arhan.ch@gmail.com>
2025-04-08 18:37:22 -0400

    prepare for multiplication program optimization

--- src/compiler/tests/multiply/multiply.c
+++ src/compiler/tests/multiply/multiply.c
@@ -4,11 +4,13 @@
 #include <time.h>
 #include <assert.h>
 
+int branch = 0;
 int count = 0;
 
 void move_const(int n, int *to)
 {
 move_const_loop:
+    branch++;
     if (*to == 0)
     {
         goto after_move_const_loop;
@@ -18,33 +20,31 @@ move_const_loop:
     goto move_const_loop;
 after_move_const_loop:
     *to += n;
+    branch++;
     if (n != 0)
         count++;
 }
 
 void move(int *from, int *to)
 {
+// TODO: rokicki exponentiation
 move_loop:
+    branch++;
     if (*from == 0)
     {
         return;
     }
     *to += 1;
-    count++;
     *from -= 1;
-    count++;
+    count += 2;
     goto move_loop;
 }
 
 void reduce_problem(int *a, int *b, int *c, const int k)
 {
-    if (*b == 0)
-    {
-        move_const(0, a);
-        return;
-    }
     *b -= 1;
     count++;
+    branch++;
     if (*b == 0)
     {
         *b += 1;
@@ -54,6 +54,7 @@ void reduce_problem(int *a, int *b, int *c, const int k)
     *b += 1;
     count++;
 reduce_b_loop:
+    branch++;
     if (*b == 0)
     {
         move(c, b);
@@ -61,18 +62,17 @@ reduce_b_loop:
     }
     *b -= k;
     *b = (*b % 30 + 30) % 30;
-    count += 2;
     *c += 1;
-    count++;
+    count += 2;
     goto reduce_b_loop;
 reduce_a_loop:
+    branch++;
     if (*a == 0)
     {
         move(c, a);
         return;
     }
     *a -= 1;
-    count++;
     *c = (*c + k) % 30;
     count += 2;
     goto reduce_a_loop;
@@ -81,125 +81,121 @@ reduce_a_loop:
 void multiply(int *a, int *b, int *c)
 {
     move_const(0, c);
+    branch++;
     if (*a == 0)
     {
         move_const(0, b);
         return;
     }
+    branch++;
     if (*b == 0)
     {
         move_const(0, a);
         return;
     }
 reduce_by_2:
-    if (*b == 0)
+    branch++;
+    if (*b % 10 == 0)
     {
         move(c, b);
         reduce_problem(a, b, c, 2);
         goto reduce_by_2;
     }
     *b -= 1;
-    count++;
     *c += 1;
-    count++;
-    if (*b == 0)
+    count += 2;
+    branch++;
+    if (*b % 10 == 0)
     {
+        move(c, b);
         goto reduce_by_3;
     }
     *b -= 1;
-    count++;
     *c += 1;
-    count++;
+    count += 2;
     goto reduce_by_2;
 reduce_by_3:
-    if (*c == 0)
+    branch++;
+    if (*b % 3 == 0)
     {
-        move(b, c);
-        reduce_problem(a, c, b, 3);
+        move(c, b);
+        reduce_problem(a, b, c, 3);
         goto reduce_by_3;
     }
-    *c -= 1;
-    count++;
-    *b += 1;
-    count++;
-    if (*c == 0)
-    {
-        goto reduce_by_5;
-    }
-    *c -= 1;
-    count++;
-    *b += 1;
-    count++;
-    if (*c == 0)
-    {
-        goto reduce_by_5;
-    }
-    *c -= 1;
-    count++;
-    *b += 1;
-    count++;
-    goto reduce_by_3;
 reduce_by_5:
-    if (*b == 0)
+    branch++;
+    if (*b % 10 == 0)
     {
         move(c, b);
         reduce_problem(a, b, c, 5);
         goto reduce_by_5;
     }
     *b -= 1;
-    count++;
     *c += 1;
-    count++;
-    if (*b == 0)
+    count += 2;
+    branch++;
+    if (*b % 10 == 0)
     {
-        goto reduce_until_1;
+        goto before_reduce_generator_7;
     }
     *b -= 1;
-    count++;
     *c += 1;
-    count++;
-    if (*b == 0)
+    count += 2;
+    branch++;
+    if (*b % 10 == 0)
     {
-        goto reduce_until_1;
+        goto before_reduce_generator_7;
     }
     *b -= 1;
-    count++;
     *c += 1;
-    count++;
-    if (*b == 0)
+    count += 2;
+    branch++;
+    if (*b % 10 == 0)
     {
-        goto reduce_until_1;
+        goto before_reduce_generator_7;
     }
     *b -= 1;
-    count++;
     *c += 1;
-    count++;
-    if (*b == 0)
+    count += 2;
+    branch++;
+    if (*b % 10 == 0)
     {
-        goto reduce_until_1;
+        goto before_reduce_generator_7;
     }
     *b -= 1;
-    count++;
     *c += 1;
-    count++;
+    count += 2;
     goto reduce_by_5;
-reduce_until_1:
-    *c -= 1;
-    count++;
-    if (*c == 0)
+before_reduce_generator_7:
+    move(c, b);
+reduce_generator_7:
+    *b -= 1;
+    branch++;
+    if (*b % 10 == 0)
+    {
+        *b += 1;
+        goto reduce_generator_11;
+    }
+    *b += 1;
+    reduce_problem(a, b, c, 7);
+    goto reduce_generator_7;
+reduce_generator_11:
+    *b -= 1;
+    branch++;
+    if (*b == 0)
     {
         return;
     }
-    *c += 1;
-    count++;
-    reduce_problem(a, c, b, 7);
-    reduce_problem(a, c, b, 11);
-    goto reduce_until_1;
+    *b += 1;
+    reduce_problem(a, b, c, 11);
+    goto reduce_generator_11;
 }
 
-typedef struct result {
+typedef struct result
+{
     char *comp;
     int count;
+    int branch;
 } result_t;
 
 int main()
@@ -215,10 +211,12 @@ int main()
             int b = j;
             int c = 0;
             count = 0;
+            branch = 0;
             multiply(&a, &b, &c);
             snprintf(buf, sizeof(buf), "%d * %d = %d", i, j, a);
             results[i * 30 + j].comp = strdup(buf);
             results[i * 30 + j].count = count;
+            results[i * 30 + j].branch = branch;
             assert(((long long)i * (long long)j) % 30 == a);
             assert(b == 0);
             assert(c == 0);
@@ -240,7 +238,7 @@ int main()
     int total = 0;
     for (int i = 0; i < 900; i++)
     {
-        printf("%s: %d additions\n", results[i].comp, results[i].count);
+        printf("%s: %d additions; %d branches\n", results[i].comp, results[i].count, results[i].branch);
         total += results[i].count;
     }
     // average count per multiplication
