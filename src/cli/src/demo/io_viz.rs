b6ad34ccf50aa6d6d9d82c04b1eb6b6dcaa20617
Henry Rovnyak <h.rovnyak@proton.me>
2025-07-20 14:14:51 -0400

    Fix text overflowing

--- src/cli/src/demo/io_viz.rs
+++ src/cli/src/demo/io_viz.rs
@@ -2,6 +2,7 @@
 use bevy::prelude::*;
 use bevy_simple_text_input::{TextInput, TextInputSubmitEvent};
 use internment::Intern;
+use itertools::Itertools;
 
 use crate::demo::interpreter_plugin::{
     BeganProgram, CommandTx, FinishedProgram, GaveInput, Input, InterpretationCommand, Message,
@@ -23,6 +24,9 @@ pub struct ExecuteButton;
 
 #[derive(Component)]
 struct MessageDisplay;
+
+#[derive(Component)]
+struct MessageBox;
 
 #[derive(Resource, Debug)]
 // struct ExecuteClicked(bool);
@@ -209,6 +213,7 @@ fn setup(mut commands: Commands, window: Single<&Window>) {
                 flex_grow: 1.,
                 ..Default::default()
             },
+            MessageBox,
             ChildOf(panel),
         ))
         .with_child((
@@ -249,9 +254,19 @@ fn started_program(
 
 fn got_message(
     mut messages: EventReader<Message>,
-    mut message_display: Single<&mut Text, With<MessageDisplay>>,
+    mut message_display: Single<(&mut Text, &TextFont), With<MessageDisplay>>,
+    message_box: Single<&ComputedNode, With<MessageBox>>,
+    window: Single<&Window>,
 ) {
     for message in messages.read() {
+        #[expect(clippy::cast_precision_loss)]
+        let height =
+            (message_display.0.0.lines().count() + 1) as f32 * message_display.1.font_size * 1.2;
+        #[expect(clippy::cast_precision_loss)]
+        if height > message_box.size().y / window.physical_height() as f32 * window.height() {
+            **message_display.0 = message_display.0.lines().skip(1).join("\n");
+        }
+
         message_display.0.push('\n');
         message_display.0.push_str(&message.0);
     }
@@ -260,17 +275,16 @@ fn got_message(
 fn on_submit(
     mut submissions: EventReader<TextInputSubmitEvent>,
     command_tx: Res<CommandTx>,
-    mut message_display: Single<&mut Text, With<MessageDisplay>>,
+    mut messages_tx: EventWriter<Message>,
 ) {
     for submission in submissions.read() {
         command_tx
             .send(InterpretationCommand::GiveInput(
                 if let Ok(v) = submission.value.parse() {
-                    message_display.push('\n');
-                    message_display.push_str(&submission.value);
+                    messages_tx.write(Message(submission.value.clone()));
                     v
                 } else {
-                    message_display.push_str("\nValue needs to be parsable as a string");
+                    messages_tx.write(Message("Value needs to be parsable as a string".to_owned()));
                     continue;
                 },
             ))
@@ -364,6 +378,6 @@ fn finished_program(
 ) {
     if finished_programs.read().last().is_some() {
         *execute_button_state = ExecuteButtonState::None;
-        ***text = "Execute".to_owned();
+        "Execute".clone_into(&mut text);
     }
 }
