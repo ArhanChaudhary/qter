1c29e0a9894e0488ecfe1f8a51d758f3d0e78e44
Henry Rovnyak <h.rovnyak@proton.me>
2025-11-14 16:45:36 -0500

    Install rob-twophase

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
           wayland # To use the wayland feature
         ];
       in
-      {
+      rec {
         devShell = pkgs.mkShell rec {
           buildInputs =
             libraries
@@ -50,6 +50,7 @@
               sccache
               rust-analyzer
               rust
+              packages.rob-twophase
 
               (gap.overrideAttrs (o: {
                 version = "4.13.1";
@@ -79,6 +80,25 @@
             export PATH=$PATH:~/.cargo/bin
           '';
         };
+
+        packages.rob-twophase = pkgs.stdenv.mkDerivation {
+          name = "rob-twophase";
+          src = pkgs.fetchFromGitHub {
+            owner = "efrantar";
+            repo = "rob-twophase";
+            rev = "d245031257d52b2663c5790c5410ef30aefd775f";
+            hash = "sha256-2QZgW7w80+oKlMFMkIvuEXdp0SkIXpLs02MHe9qjb/c=";
+          };
+          buildPhase = ''
+            make
+          '';
+          installPhase = ''
+             mkdir -p $out/bin
+             cp twophase $out/bin
+          '';
+        };
+        
+        legacyPackages = packages;
       }
     );
 }
